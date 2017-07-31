69e4810039152b3e4a1e68fa47313c43f7458b70
Sukhdev Mohan <lordshivablackhacker@gmail.com>
2017-08-01 00:42:01 +0200

    cfa.py

---
+++ cfa.py
@@ -0,0 +1,22 @@
+#continuous fraction algorithm
+
+from __future__ import division #for python 2.7
+import math
+
+i = 0
+a = input("insert a : ")
+b = input("insert b : ")
+
+x = []
+q = []
+
+x.append(a/b)
+q.append(math.floor(x[0]))
+
+while q[i] != x[i]:
+    x.append(1/(x[i] - q[i]))
+    q.append(math.floor(x[i+1]))
+    print str(i) + ": " + str(x[i]) + "--" + str(q[i])
+    i = i + 1
+
+q
